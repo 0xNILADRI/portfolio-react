fc38fd6474ba9b1f474402d95ae5a506558c8fc9
Niladri Ghosh <niladri1406@gmail.com>
2024-02-18 19:52:24 +0530

    add: added illustrations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,21 @@
+import "../index.css";
 function Hero() {
   return (
-    <section class="hero">
-      <div class="middle-col">
-        <p class="my-name uppercase primary-color">Niladri Ghosh</p>
-        <h1 class="secondary-color">Designer. Developer.</h1>
-        <a href="#" class="know-more-button uppercase">
-          Know More
-        </a>
+    <>
+      <section class="hero">
+        <div class="middle-col">
+          <p class="my-name uppercase primary-color">Niladri Ghosh</p>
+          <h1 class="secondary-color">Designer. Developer.</h1>
+          <a href="#" class="know-more-button uppercase">
+            Know More
+          </a>
+        </div>
+      </section>
+
+      <div class="hero-illustration">
+        <img class="bottom-illustration" alt="Bottom Illustration" />
       </div>
-    </section>
+    </>
   );
 }
 export default Hero;
