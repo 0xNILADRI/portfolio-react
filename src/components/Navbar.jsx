5dd1258aaf61ae878c811807a91bbc56d25a12ff
Niladri Ghosh <niladri1406@gmail.com>
2024-02-18 23:36:55 +0530

    fix: functional navbar button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { navBarLinks } from "../data";
 import navMenu from "../assets/menu.svg";
 import navCloseMenu from "../assets/menu-close.svg";
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = (e) => {
+    setIsActive((current) => !current);
+  };
+
   return (
     <div className="navbar">
       <div className="container">
@@ -15,14 +21,17 @@ function Navbar() {
           className="mobile-menu"
           src={navMenu}
           alt="Open Navigation Menu"
+          onClick={handleClick}
+          style={{ display: isActive ? "none" : "block" }}
         />
 
-        <nav>
+        <nav className={isActive ? "mobile-btn" : ""}>
           <img
             id="mobile-exit"
             className="mobile-menu-exit"
             src={navCloseMenu}
             alt="Close Navigation Menu"
+            onClick={handleClick}
           />
           <ul className="main-nav">
             {navBarLinks.map(({ key, href, className, text }) => {
