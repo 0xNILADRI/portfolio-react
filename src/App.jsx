6f531c91bf74c82a06f1e665b9434f8e1db617c0
Niladri Ghosh <niladri1406@gmail.com>
2024-02-18 19:43:06 +0530

    add: middle section for intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 function App() {
   return (
     <>
       {/* <Loader /> */}
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <section class="hero">
+      <div class="middle-col">
+        <p class="my-name uppercase primary-color">Niladri Ghosh</p>
+        <h1 class="secondary-color">Designer. Developer.</h1>
+        <a href="#" class="know-more-button uppercase">
+          Know More
+        </a>
+      </div>
+    </section>
+  );
+}
+export default Hero;
